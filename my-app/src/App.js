9dea415f61832d9bd2082bf12e97932b1986516a
thduck284 <0365946542az@gmail.com>
2025-08-18 13:36:00 +0700

    project

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App">
+          <h1>Thông tin cá nhân</h1>
+          <p>Họ tên: Tô Hữu Đức</p>
+          <p>MSSV: 22110311</p>
+          <p>Lớp: 22110ST2B</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
